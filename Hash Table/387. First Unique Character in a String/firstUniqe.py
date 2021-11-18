ef1fc360fe6ec3e402ebe09b987ada4de2d728ea
Travis <42667524+TravisLeeWolf@users.noreply.github.com>
2021-11-18 11:18:48 +0900

    first unique char

--- Hash Table/387. First Unique Character in a String/firstUniqe.py
+++ Hash Table/387. First Unique Character in a String/firstUniqe.py
@@ -1,3 +1,17 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        pass
+        occuranceMap = {}
+        for letter in s:
+            if letter not in occuranceMap.keys():
+                occuranceMap[letter] = 1
+            else:
+                occuranceMap[letter] += 1
+
+
+        for key in occuranceMap:
+            if occuranceMap[key] == 1:
+                return s.index(key)
+        return -1
+
+check = Solution()
+print(check.firstUniqChar(s="aabb"))
